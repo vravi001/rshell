1e926ddcbed2a0b9bbac6aa6f6e569787bec9407
Vinantika Ravinder <vravi001@hammer.cs.ucr.edu>
2016-02-11 18:02:12 -0800

    added parsing to main function in a sentinel while loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,32 @@
 //this is our main file
 // testing
+#include <iostream>
+#include <boost/tokenizer.hpp>
+#include <string>
+using namespace std;
+int main()
+{
+	vector<string> parse;
+	string str;
+	cout << "$";
+	getline(cin, str);
+	cout << endl;
+	while(str != "exit")
+	{
+		typedef boost::toknizer<boost::char_separator<char> > Tok;
+		boost::char_separator<shar> sep("", "|&#;");
+		Tok tok(str, sep);
+		for(Tok::iterator tok_iter = tok.begin(); tok_iter != tok.end(); ++tok_iter)
+		{
+			parse.push_back(*tok_iter);
+		}
+		//do class work here
+		parse.clear();
+		cout << "$";
+		getline(cin, str);
+		cout << endl;
+	}
+
+	return 0;
+}
+
