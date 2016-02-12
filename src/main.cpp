9117ee40ce9bbbbbb7ff82607ebcb2631cd3db07
Vinantika Ravinder <vravi001@hammer.cs.ucr.edu>
2016-02-11 22:48:22 -0800

    Fixed compilation errors and coded connector and execute functions

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,27 @@ using namespace std;
 void execute(string command, bool &pass) {
 	int pos = command.find(" ");
 	string c1 = command.substr(pos);
-	string c2 = command.substr(0, pos);
+	string c0 = command.substr(0, pos);
+
+	//const char *x = c1.c_str();
+	//const char *y = c0.c_str();
+	
+	char *x = new char[c1.size() + 1];
+	copy(c1.begin(), c1.end(), x);
+	x[c1.size()] = '\0';
+	
+	char *y = new char(c0.size() + 1);
+	copy(c0.begin(), c0.end(), y);
+	y[c0.size()] = '\0';
+
 
 	vector<char *> v;
-	v.push_back(c0);
-	v.push_back(c1);
-	
-	char **cmd = &v[0];
+	//v.push_back(y); //
+	v.push_back(y);
+	v.push_back(x);
+	//v.push_back(NULL);
+	//char **cmd = &v[0];
+	char *cmd[] = {y, x, NULL};
 	int status = execvp(v[0], cmd);
 	if (status == -1) {
 		pass = false;
@@ -27,6 +41,9 @@ void execute(string command, bool &pass) {
 	else {
 		pass = true;
 	}
+	cout << "Status of " << command << " : " << status << endl;
+	delete[] x;
+	delete[] y;
 }
 
 void connector(vector<string> parse)
@@ -35,6 +52,7 @@ void connector(vector<string> parse)
 	int i = 1;
 	string exec = parse.at(0);
 	execute(exec, pass);
+	cout << "Hello" << endl;
 	while(i < parse.size())
 	{
 		if(parse.at(i) == "&")
@@ -60,6 +78,10 @@ void connector(vector<string> parse)
 			execute(exec, pass);
 			i = i + 2;
 		}
+		else if(parse.at(i) == "#")
+		{
+			i = parse.size();
+		}
 	}
 	return;
 }
@@ -69,7 +91,7 @@ int main()
 	char hostname[150];
 	gethostname(hostname, sizeof hostname);
 
-	vector<string> parse;
+	//vector<string> parse;
 	string str;
 	cout << getlogin() << "@" << hostname << "$ ";
 	//cout << "$";
@@ -77,6 +99,8 @@ int main()
 	cout << endl;
 	while(str != "exit")
 	{
+		vector<string> parse;
+		//vector<string> mod;
 		typedef boost::tokenizer<boost::char_separator<char> > Tok;
 		boost::char_separator<char> sep("", "|&#;");
 		Tok tok(str, sep);
@@ -84,6 +108,21 @@ int main()
 		{
 			parse.push_back(*tok_iter);
 		}
+		//int min = 0;
+		//int temp = 0;
+		//for(int i = 0; i < parse.size(); ++i)
+		//{
+		//	if(parse.at(i) == "#")
+		//	{
+		//		temp = i;
+		//	}
+		//	if(temp < min)
+		//	{
+		//		min = temp;
+		//	}
+		//}
+
+		connector(parse);
 		//do class work here
 		parse.clear();
 		cout << getlogin() << "@" << hostname << "$ ";
