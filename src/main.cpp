f44d7975ae78aaf7b30b96b56944b061647e5994
Vinantika Ravinder <vravi001@hammer.cs.ucr.edu>
2016-02-12 16:39:13 -0800

    Fixed complications with execvp, and introduced fork and waitpid

--- src/main.cpp
+++ src/main.cpp
@@ -6,42 +6,59 @@
 #include <iostream>
 #include <boost/tokenizer.hpp>
 #include <string>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <cstring>
 using namespace std;
 
 
 void execute(string command, bool &pass) {
+	if(command == "exit")
+	{
+		exit(1);
+	}
 	int pos = command.find(" ");
 	string c1 = command.substr(pos);
-	string c0 = command.substr(0, pos);
-
-	//const char *x = c1.c_str();
-	//const char *y = c0.c_str();
-	
+	string c0 = command.substr(0, pos); //see if +1 fixed out of range error
+	//c0.erase(remove(c0.begin(), c0.end(), ' '), c0.end());
+	c1.erase(remove(c1.begin(), c1.end(), ' '), c1.end());
 	char *x = new char[c1.size() + 1];
 	copy(c1.begin(), c1.end(), x);
-	x[c1.size()] = '\0';
-	
+	x[c1.size()] = '\0'; //took out this
+
 	char *y = new char(c0.size() + 1);
 	copy(c0.begin(), c0.end(), y);
-	y[c0.size()] = '\0';
-
+	y[c0.size()] = '\0'; //took out this
 
 	vector<char *> v;
-	//v.push_back(y); //
 	v.push_back(y);
-	v.push_back(x);
-	//v.push_back(NULL);
-	//char **cmd = &v[0];
-	char *cmd[] = {y, x, NULL};
-	int status = execvp(v[0], cmd);
-	if (status == -1) {
-		pass = false;
+	v.push_back(x); 
 
+	char *cmd[] = {y, x, NULL};
+	pid_t pid;
+	int status;
+	int cStatus;
+	if((pid = fork())< 0)
+	{
+		cout << "Error: failed forking child process" << endl;
+		exit(1);
 	}
-	else {
-		pass = true;
+	else if(pid == 0)
+	{
+		status = execvp(y, cmd);
+		if(status < 0)
+		{
+			pass = false;
+		}
+		else{
+			pass = true;
+		}
 	}
-	cout << "Status of " << command << " : " << status << endl;
+	else{
+		while(wait(&cStatus) !=pid)
+		{
+		}
+	}
 	delete[] x;
 	delete[] y;
 }
@@ -49,12 +66,15 @@ void execute(string command, bool &pass) {
 void connector(vector<string> parse)
 {
 	bool pass = true;
-	int i = 1;
-	string exec = parse.at(0);
-	execute(exec, pass);
-	cout << "Hello" << endl;
+	int i = 0;
+	string exec = parse.at(i);
+	execute(exec, pass); //FIX THIS
+	//cout << "Hello" << endl;
+	//cout << "Parse vector size: " << parse.size() << endl;
+	i = i+1;
 	while(i < parse.size())
 	{
+		//cout << "Connector while loop" << endl;
 		if(parse.at(i) == "&")
 		{
 			if(pass)
@@ -80,7 +100,7 @@ void connector(vector<string> parse)
 		}
 		else if(parse.at(i) == "#")
 		{
-			i = parse.size();
+			i = parse.size()+ 1;
 		}
 	}
 	return;
@@ -99,32 +119,21 @@ int main()
 	cout << endl;
 	while(str != "exit")
 	{
+		//cout << "In while loop" << endl;
 		vector<string> parse;
 		//vector<string> mod;
 		typedef boost::tokenizer<boost::char_separator<char> > Tok;
-		boost::char_separator<char> sep("", "|&#;");
+		boost::char_separator<char> sep("", "|&#;"); //see if trailing newline is a problem
 		Tok tok(str, sep);
 		for(Tok::iterator tok_iter = tok.begin(); tok_iter != tok.end(); ++tok_iter)
 		{
 			parse.push_back(*tok_iter);
 		}
-		//int min = 0;
-		//int temp = 0;
-		//for(int i = 0; i < parse.size(); ++i)
-		//{
-		//	if(parse.at(i) == "#")
-		//	{
-		//		temp = i;
-		//	}
-		//	if(temp < min)
-		//	{
-		//		min = temp;
-		//	}
-		//}
 
 		connector(parse);
 		//do class work here
-		parse.clear();
+		//parse.clear();
+		//cout << "In while loop 2" << endl;
 		cout << getlogin() << "@" << hostname << "$ ";
 		//cout << "$";
 		getline(cin, str);
