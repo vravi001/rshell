65324ba3d016c7d86bad20b7113375ab3c686269
Yohoverlit Bernal Diaz <ybern004@hammer.cs.ucr.edu>
2016-02-12 19:28:20 -0800

    created script files and moved to tests directory

---
+++ src/tests/exit.sh
@@ -0,0 +1,95 @@
+Script started on Fri 12 Feb 2016 06:43:40 PM PST
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[?1034h[ybern004@hammer src]$ g++ main.cpp
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ ./a.out
+ybern004@hammer.cs.ucr.edu$ exit;  ls -a
+
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ ./a.outg++ main.cpp[5P./a.out
+ybern004@hammer.cs.ucr.edu$ mkdir helo lo%%  &&touch hello;exit
+
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ exi[K[K[K./a.out
+ybern004@hammer.cs.ucr.edu$ exit
+
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ ./a.out
+ybern004@hammer.cs.ucr.edu$ ls -a;git status&&exit
+
+.   a.out		  exit.sh  fileee2  hello   main.cpp	      read.txt	see.txt		   tesst   test  test2	test33	  testt   test.txt  this.txt
+..  commented_command.sh  filee2   .git     if.txt  multi_command.sh  sctest	single_command.sh  test   test1  test3	testfile  testtt  thisdir   win
+# On branch master
+# Changed but not updated:
+#   (use "git add <file>..." to update what will be committed)
+#   (use "git checkout -- <file>..." to discard changes in working directory)
+#
+#	modified:   main.cpp
+#
+# Untracked files:
+#   (use "git add <file>..." to include in what will be committed)
+#
+#	a.out
+#	commented_command.sh
+#	exit.sh
+#	filee2
+#	fileee2
+#	if.txt
+#	multi_command.sh
+#	read.txt
+#	see.txt
+#	single_command.sh
+#	test.txt
+#	testfile
+#	this.txt
+no changes added to commit (use "git add" and/or "git commit -a")
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ ./a.out
+ybern004@hammer.cs.ucr.edu$ echo hello;exit;echo world
+
+hello
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ ./a.out
+ybern004@hammer.cs.ucr.edu$ ls -l; &&mkdir testt5;exit
+
+total 132
+-rwxr-xr-x 1 ybern004 classes 56134 Feb 12 18:43 a.out
+-rw-r--r-- 1 ybern004 classes  5717 Feb 12 18:42 commented_command.sh
+-rw-r--r-- 1 ybern004 classes     0 Feb 12 18:43 exit.sh
+-rw-r--r-- 1 ybern004 classes     0 Feb 12 18:39 filee2
+-rw-r--r-- 1 ybern004 classes     0 Feb 12 18:41 fileee2
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 18:44 hello
+-rw-r--r-- 1 ybern004 classes     0 Feb 12 18:31 if.txt
+-rw-r--r-- 1 ybern004 classes  2770 Feb 12 16:44 main.cpp
+-rw-r--r-- 1 ybern004 classes  4378 Feb 12 18:32 multi_command.sh
+-rw-r--r-- 1 ybern004 classes     0 Feb 12 18:37 read.txt
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 18:19 sctest
+-rw-r--r-- 1 ybern004 classes     0 Feb 12 18:31 see.txt
+-rw-r--r-- 1 ybern004 classes  3417 Feb 12 18:20 single_command.sh
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 18:39 tesst
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 18:35 test
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 15:37  test
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 18:27 test1
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 18:29 test2
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 18:30 test3
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 18:37 test33
+-rw-r--r-- 1 ybern004 classes     0 Feb 12 18:36 testfile
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 18:22 testt
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 18:26 testtt
+-rw-r--r-- 1 ybern004 classes     0 Feb 12 18:35 test.txt
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 18:20 thisdir
+-rw-r--r-- 1 ybern004 classes     0 Feb 12 18:31 this.txt
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 16:04 win
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ ./a.out
+ybern004@hammer.cs.ucr.edu$ echo testinf g&&echo exit&&exit;echo command
+
+testing
+exit
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ ./a.out
+ybern004@hammer.cs.ucr.edu$ exit
+
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ ./a.out
+ybern004@hammer.cs.ucr.edu$ touch check&&exit
+
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ ./a.out
+ybern004@hammer.cs.ucr.edu$ mkdir test6&& & tou    touch file_exit;ls -a&&exit;git status
+
+.   a.out  commented_command.sh  filee2   file_exit  hello   main.cpp	       read.txt  see.txt	    tesst   test  test2  test33  testfile  testt5  test.txt  this.txt
+..  check  exit.sh		 fileee2  .git	     if.txt  multi_command.sh  sctest	 single_command.sh  test   test1  test3  test6	 testt	   testtt  thisdir   win
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ exit
+exit
+
+Script done on Fri 12 Feb 2016 06:47:27 PM PST
---
+++ src/tests/single_command.sh
@@ -0,0 +1,99 @@
+Script started on Fri 12 Feb 2016 06:18:30 PM PST
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[?1034h[ybern004@hammer src]$ g++ main.cpp
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ ./a.out
+ybern004@hammer.cs.ucr.edu$ ls -a
+
+.  ..  a.out  .git  main.cpp  single_command.sh   test	txt.txt  win
+ybern004@hammer.cs.ucr.edu$ ls -a l
+
+total 68
+-rwxr-xr-x 1 ybern004 classes 56134 Feb 12 18:18 a.out
+-rw-r--r-- 1 ybern004 classes  2770 Feb 12 16:44 main.cpp
+-rw-r--r-- 1 ybern004 classes     0 Feb 12 18:18 single_command.sh
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 15:37  test
+-rw-r--r-- 1 ybern004 classes     0 Feb 12 18:16 txt.txt
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 16:04 win
+ybern004@hammer.cs.ucr.edu$ echo hi
+
+hi
+ybern004@hammer.cs.ucr.edu$ git status
+
+# On branch master
+# Changed but not updated:
+#   (use "git add <file>..." to update what will be committed)
+#   (use "git checkout -- <file>..." to discard changes in working directory)
+#
+#	modified:   main.cpp
+#
+# Untracked files:
+#   (use "git add <file>..." to include in what will be committed)
+#
+#	a.out
+#	single_command.sh
+#	txt.txt
+no changes added to commit (use "git add" and/or "git commit -a")
+ybern004@hammer.cs.ucr.edu$ mkdir sctest
+
+ybern004@hammer.cs.ucr.edu$ ls -a
+
+.  ..  a.out  .git  main.cpp  sctest  single_command.sh   test	txt.txt  win
+ybern004@hammer.cs.ucr.edu$ ls
+
+terminate called after throwing an instance of 'std::out_of_range'
+  what():  basic_string::substr
+Aborted
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ ./a.outg++ main.cpp[5P./a.out
+ybern004@hammer.cs.ucr.edu$ echo Hello
+
+Hello
+ybern004@hammer.cs.ucr.edu$ ls -a
+
+.  ..  a.out  .git  main.cpp  sctest  single_command.sh   test	txt.txt  win
+ybern004@hammer.cs.ucr.edu$ ls -l
+
+total 72
+-rwxr-xr-x 1 ybern004 classes 56134 Feb 12 18:18 a.out
+-rw-r--r-- 1 ybern004 classes  2770 Feb 12 16:44 main.cpp
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 18:19 sctest
+-rw-r--r-- 1 ybern004 classes     0 Feb 12 18:18 single_command.sh
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 15:37  test
+-rw-r--r-- 1 ybern004 classes     0 Feb 12 18:16 txt.txt
+drwxr-xr-x 2 ybern004 classes  4096 Feb 12 16:04 win
+ybern004@hammer.cs.ucr.edu$ echo world
+
+world
+ybern004@hammer.cs.ucr.edu$ git status
+
+# On branch master
+# Changed but not updated:
+#   (use "git add <file>..." to update what will be committed)
+#   (use "git checkout -- <file>..." to discard changes in working directory)
+#
+#	modified:   main.cpp
+#
+# Untracked files:
+#   (use "git add <file>..." to include in what will be committed)
+#
+#	a.out
+#	single_command.sh
+#	txt.txt
+no changes added to commit (use "git add" and/or "git commit -a")
+ybern004@hammer.cs.ucr.edu$ mkdir thisdir
+
+ybern004@hammer.cs.ucr.edu$ ls -a
+
+.  ..  a.out  .git  main.cpp  sctest  single_command.sh   test	thisdir  txt.txt  win
+ybern004@hammer.cs.ucr.edu$ touch testing.txt
+
+ybern004@hammer.cs.ucr.edu$ ls -a
+
+.  ..  a.out  .git  main.cpp  sctest  single_command.sh   test	testing.txt  thisdir  txt.txt  win
+ybern004@hammer.cs.ucr.edu$ ls
+
+terminate called after throwing an instance of 'std::out_of_range'
+  what():  basic_string::substr
+Aborted
+]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ exit
+exit
+
+Script done on Fri 12 Feb 2016 06:20:45 PM PST
