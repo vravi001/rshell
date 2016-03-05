6f89f0df9e089dfcb1896c2574b6e266b9d04a68
Yohoverlit Bernal Diaz <ybern004@hammer.cs.ucr.edu>
2016-03-04 20:37:23 -0800

    created test scripts and added to tests directory

--- src/tests/commented_command.sh
+++ src/tests/commented_command.sh
@@ -1,163 +1,47 @@
-Script started on Fri 12 Feb 2016 06:33:41 PM PST
-]0;ybern004@hammer:~/CS-100/assign2/rshell/src[?1034h[ybern004@hammer src]$ g ++[K[K[K++ main.cpp
-]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ ./a.out
-ybern004@hammer.cs.ucr.edu$ ls -a;echo hello## echo world
+Script started on Fri 04 Mar 2016 08:13:51 PM PST
+]0;ybern004@hammer:~/CS-100/assign3/rshell/src[?1034h[ybern004@hammer src]$ make
+g++ -Wall -Werror -ansi -pedantic rshell.cpp -o rshell.out
+]0;ybern004@hammer:~/CS-100/assign3/rshell/src[ybern004@hammer src]$ makeexit./rshell.out
+ybern004@hammer.cs.ucr.edu$ #echo a&&echo a b
 
-.  ..  a.out  commented_command.sh  .git  if.txt  main.cpp  multi_command.sh  sctest  see.txt  single_command.sh   test  test1	test2  test3  testt  testtt  thisdir  this.txt	win
-hello
-ybern004@hammer.cs.ucr.edu$ mkdir test&&touch test.txt;ls -a#echo hello
+ybern004@hammer.cs.ucr.edu$ ls -a&&echo b#echo c
 
-.  ..  a.out  commented_command.sh  .git  if.txt  main.cpp  multi_command.sh  sctest  see.txt  single_command.sh  test	 test  test1  test2  test3  testt  testtt  test.txt  thisdir  this.txt	win
-ybern004@hammer.cs.ucr.edu$ git status&&echo hello&&  #mkdir test2.txt    2.txt
+.   a.out		  main.cpp  precedence_test.sh	rshell.out  test_test.sh
+..  commented_command.sh  makefile  rshell.cpp		tests
+b
+ybern004@hammer.cs.ucr.edu$ #[ -e /class/classes/ybern004/CS-100/assign3/rshell/src ]
 
-# On branch master
-# Changed but not updated:
-#   (use "git add <file>..." to update what will be committed)
-#   (use "git checkout -- <file>..." to discard changes in working directory)
-#
-#	modified:   main.cpp
-#
-# Untracked files:
-#   (use "git add <file>..." to include in what will be committed)
-#
-#	a.out
-#	commented_command.sh
-#	if.txt
-#	multi_command.sh
-#	see.txt
-#	single_command.sh
-#	test.txt
-#	this.txt
-no changes added to commit (use "git add" and/or "git commit -a")
-hello
-ybern004@hammer.cs.ucr.edu$ touch testfile;echo hello&&ls -a#git status
+ybern004@hammer.cs.ucr.edu$ [ -e /class/classes/ybern004/CS-100/assign3/rshell/src ]&&echo a#echo b
 
-hello
-.   a.out		  .git	  main.cpp	    sctest   single_command.sh	 test  test2  testfile	testtt	  thisdir   win
-..  commented_command.sh  if.txt  multi_command.sh  see.txt  test		test1  test3  testt	test.txt  this.txt
-ybern004@hammer.cs.ucr.edu$ echo testing&&echo finise hed#ls -a
+(True)
+a
+ybern004@hammer.cs.ucr.edu$ #exit
 
-testing
-finished
-ybern004@hammer.cs.ucr.edu$ mkdir est   test33&&touch read.txt&&git status#ls -a l
+ybern004@hammer.cs.ucr.edu$ exit#echo a
 
-# On branch master
-# Changed but not updated:
-#   (use "git add <file>..." to update what will be committed)
-#   (use "git checkout -- <file>..." to discard changes in working directory)
-#
-#	modified:   main.cpp
-#
-# Untracked files:
-#   (use "git add <file>..." to include in what will be committed)
-#
-#	a.out
-#	commented_command.sh
-#	if.txt
-#	multi_command.sh
-#	read.txt
-#	see.txt
-#	single_command.sh
-#	test.txt
-#	testfile
-#	this.txt
-no changes added to commit (use "git add" and/or "git commit -a")
-ybern004@hammer.cs.ucr.edu$ ls -a;git status#touch file1
+]0;ybern004@hammer:~/CS-100/assign3/rshell/src[ybern004@hammer src]$ ./rshell.out
+ybern004@hammer.cs.ucr.edu$ echo a&&(echo b#echo c)
 
-.   a.out		  .git	  main.cpp	    read.txt  see.txt		 test	test1  test3   testfile  testtt    thisdir   win
-..  commented_command.sh  if.txt  multi_command.sh  sctest    single_command.sh   test	test2  test33  testt	 test.txt  this.txt
-# On branch master
-# Changed but not updated:
-#   (use "git add <file>..." to update what will be committed)
-#   (use "git checkout -- <file>..." to discard changes in working directory)
-#
-#	modified:   main.cpp
-#
-# Untracked files:
-#   (use "git add <file>..." to include in what will be committed)
-#
-#	a.out
-#	commented_command.sh
-#	if.txt
-#	multi_command.sh
-#	read.txt
-#	see.txt
-#	single_command.sh
-#	test.txt
-#	testfile
-#	this.txt
-no changes added to commit (use "git add" and/or "git commit -a")
-ybern004@hammer.cs.ucr.edu$ echo hello;echo world&&git status#echo testinf g
+a
+b
+ybern004@hammer.cs.ucr.edu$ (#echo a)||echo b
 
-hello
-world
-# On branch master
-# Changed but not updated:
-#   (use "git add <file>..." to update what will be committed)
-#   (use "git checkout -- <file>..." to discard changes in working directory)
-#
-#	modified:   main.cpp
-#
-# Untracked files:
-#   (use "git add <file>..." to include in what will be committed)
-#
-#	a.out
-#	commented_command.sh
-#	if.txt
-#	multi_command.sh
-#	read.txt
-#	see.txt
-#	single_command.sh
-#	test.txt
-#	testfile
-#	this.txt
-no changes added to commit (use "git add" and/or "git commit -a")
-ybern004@hammer.cs.ucr.edu$ mkdir tesst&&touch filee2;ls -a&&git status          #git status
+ybern004@hammer.cs.ucr.edu$ ls -a  #[ -e /class/classes/ybern004/CS-100/assign3/rshell/src ]
 
-.   a.out		  filee2  if.txt    multi_command.sh  sctest   single_command.sh  test	 test1	test3	testfile  testtt    thisdir   win
-..  commented_command.sh  .git	  main.cpp  read.txt	      see.txt  tesst		   test  test2	test33	testt	  test.txt  this.txt
-terminate called after throwing an instance of 'std::out_of_range'
-  what():  basic_string::substr
-Aborted
-]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ ./a.outg++ main.cpp[8Pexit./a.out
-ybern004@hammer.cs.ucr.edu$ ^[[A^[[A        mkdirr  test&&touch fileee2;ls -a&&git status#echo ts esting
+.   a.out		  main.cpp  precedence_test.sh	rshell.out  test_test.sh
+..  commented_command.sh  makefile  rshell.cpp		tests
+ybern004@hammer.cs.ucr.edu$ [   (ech a   o   ho a&&echo b)#echo c
 
-mkdir: cannot create directory `test': File exists
-.   a.out		  filee2   .git    main.cpp	     read.txt  see.txt		  tesst   test	test2  test33	 testt	 test.txt  this.txt
-..  commented_command.sh  fileee2  if.txt  multi_command.sh  sctest    single_command.sh  test	 test1	test3  testfile  testtt  thisdir   win
-# On branch master
-# Changed but not updated:
-#   (use "git add <file>..." to update what will be committed)
-#   (use "git checkout -- <file>..." to discard changes in working directory)
-#
-#	modified:   main.cpp
-#
-# Untracked files:
-#   (use "git add <file>..." to include in what will be committed)
-#
-#	a.out
-#	commented_command.sh
-#	filee2
-#	fileee2
-#	if.txt
-#	multi_command.sh
-#	read.txt
-#	see.txt
-#	single_command.sh
-#	test.txt
-#	testfile
-#	this.txt
-no changes added to commit (use "git add" and/or "git commit -a")
-ybern004@hammer.cs.ucr.edu$ echo testinf f g  &&echo comments&&echo is&& echo complete#ls -a
+a
+b
+ybern004@hammer.cs.ucr.edu$ (#)
 
-testing
-comments
-is
+ybern004@hammer.cs.ucr.edu$ test - e  e /class/classes/ybern004/CS-100/assign3/rshell/src#echo a
+
+(True)
 ybern004@hammer.cs.ucr.edu$ exit
 
-ybern004@hammer.cs.ucr.edu$ exit
-
-]0;ybern004@hammer:~/CS-100/assign2/rshell/src[ybern004@hammer src]$ exit
+]0;ybern004@hammer:~/CS-100/assign3/rshell/src[ybern004@hammer src]$ exit
 exit
 
-Script done on Fri 12 Feb 2016 06:42:34 PM PST
+Script done on Fri 04 Mar 2016 08:17:09 PM PST
---
+++ src/tests/precedence_test.sh
@@ -0,0 +1,65 @@
+Script started on Fri 04 Mar 2016 08:06:32 PM PST
+]0;ybern004@hammer:~/CS-100/assign3/rshell/src[?1034h[ybern004@hammer src]$ make
+g++ -Wall -Werror -ansi -pedantic rshell.cpp -o rshell.out
+]0;ybern004@hammer:~/CS-100/assign3/rshell/src[ybern004@hammer src]$ ./rshell.cpp[K[K[Kout
+ybern004@hammer.cs.ucr.edu$ (test -e /class/classes/ybern004/CS-100/assign3/rshell/src)||(test -e /class/classes/ybern004/CS-100/assign3/rshell/src)
+
+(True)
+ybern004@hammer.cs.ucr.edu$ (test -e /class/classes/ybern004/CS-100/assign3/rshell/src)&&(test -e /class/classes/ybern004/CS-100/assign3/rshell/src)
+
+(True)
+(True)
+ybern004@hammer.cs.ucr.edu$ ((test -e /class/classes/ybern004/CS-100/assign3/rshell/src)&&(test -e /class/classes/ybern004/CS-100/assign3/rshell/src))||test -e /class/classes/ybern004/CS-100/assign3/rshell/src
+
+(True)
+(True)
+ybern004@hammer.cs.ucr.edu$ ((echo a&&echo b)||echo c)&&echo d
+
+a
+b
+d
+ybern004@hammer.cs.ucr.edu$ ((echo a&&echo b)e ||echo c)||echo d
+
+a
+b
+ybern004@hammer.cs.ucr.edu$ (((echoa   a    a&&echo b)||cho c     echo c)&&echo d)&&echo e
+
+a
+b
+d
+e
+ybern004@hammer.cs.ucr.edu$ (((echo a&&echo b)&&echo c)&& e  echo d)||echo e
+
+a
+b
+c
+d
+ybern004@hammer.cs.ucr.edu$ (ech a  o a)&&(echo b)
+
+a
+b
+ybern004@hammer.cs.ucr.edu$ ((echo a)&&(echo b))
+
+a
+b
+ybern004@hammer.cs.ucr.edu$ echo a&&(echo c b||echo c)
+
+a
+b
+ybern004@hammer.cs.ucr.edu$ echo a&&ec  (echo b&&(echo c))
+
+a
+b
+c
+ybern004@hammer.cs.ucr.edu$ echo a&&(echo b&&(echo c||echo d)&&echo e)
+
+a
+b
+c
+e
+ybern004@hammer.cs.ucr.edu$ exit
+
+]0;ybern004@hammer:~/CS-100/assign3/rshell/src[ybern004@hammer src]$ exit
+exit
+
+Script done on Fri 04 Mar 2016 08:13:24 PM PST
